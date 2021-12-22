c86d974bd6cf2343be0b4302080de3ca39d01e7e
Sergei Krepski <sergei@krepski.ru>
2021-12-22 23:29:23 +0300

    Add comments

--- app/logger.py
+++ app/logger.py
@@ -10,6 +10,7 @@ _LOG_FORMAT = f'%(asctime)s - [%(levelname)s] - %(name)s : %(message)s'
 
 
 def _get_file_handler() -> logging.FileHandler:
+    """Create file handler"""
     file_handler = logging.FileHandler(path.join(getcwd(), 'error.log'))
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(logging.Formatter(_LOG_FORMAT))
@@ -17,6 +18,7 @@ def _get_file_handler() -> logging.FileHandler:
 
 
 def _get_stream_handler() -> logging.FileHandler:
+    """Create stream handler"""
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(logging.Formatter(_LOG_FORMAT))
@@ -24,6 +26,7 @@ def _get_stream_handler() -> logging.FileHandler:
 
 
 def get_logger(name) -> logging.Logger:
+    """Return logger for module. Use 'get_logger(__name__)' in code."""
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(_get_file_handler())
