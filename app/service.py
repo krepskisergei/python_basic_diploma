77c77286023edeacbb07c97bbd5399310e63578d
Sergei Krepski <sergei@krepski.ru>
2022-09-28 22:39:19 +0300

    Add handler processes
    command
    locationId
    checkIn

--- app/service.py
+++ app/service.py
@@ -1,33 +1,149 @@
+from datetime import datetime, timedelta
+from telebot.types import ReplyKeyboardMarkup
+from telebot.types import InlineKeyboardMarkup
+from telegram_bot_calendar import WMonthTelegramCalendar
+from telegram_bot_calendar import LSTEP
+from dataclasses import dataclass
+
 from app.logger import get_logger
 from classes.history import History
 from db.database import Database
+from api.rapidapi import RapidApi
 from app.config import DB_ENGINE
+import bot.messages as m
+
+
+@dataclass(frozen=True)
+class MsgKeyboard:
+    msg: str
+    markup: ReplyKeyboardMarkup | InlineKeyboardMarkup = None
 
 
 # initiate
 logger = get_logger(__name__)
 db = Database(DB_ENGINE)
+api = RapidApi()
 
 
-def get_history(
-        chat_id: int, create: bool = False, command: str = '', ) -> History:
+def get_history(chat_id: int, command: str = None) -> History:
     """Return existing or create History."""
     h = db.get_current_history(chat_id)
-    if h is None and create:
+    if h is None and command:
         # create new history
         h = History(chat_id)
-        if command:
-            h.set_attributes({'command': command})
-            db_h = db.add_history(h)
-            return db_h
-        logger.error('Error creating History: no command.')
+        h.set_attributes({'command': command})
+        return db.add_history(h)
     else:
         return h
 
 
-def process_command(chat_id: int, command: str):
+def process_command(
+        chat_id: int, message: str = None) -> tuple[list[MsgKeyboard], str]:
+    """Process user message. Return MsgKeyboard list and next handler."""
+    commands = (
+        '/lowprice',
+        '/highprice',
+        '/bestdeal'
+    )
+    command = message if message in commands else False
     h = get_history(chat_id, command)
-    if h.current_step == 'locationId':
-        print(h.command)
+    if h is None:
+        return ([MsgKeyboard(m.BAD_CONTENT_MESSAGE)], None)
+    match h.current_step:
+        case 'locationId':
+            if command:
+                messages = []
+                match command:
+                    case '/lowprice':
+                        messages.append(MsgKeyboard(m.LOWPRICE_MESSAGE))
+                    case '/highprice':
+                        messages.append(MsgKeyboard(m.HIGHPRICE_MESSSAGE))
+                    case '/bestdeal':
+                        messages.append(MsgKeyboard(m.BESTDEAL_MESSAGE))
+                messages.append(MsgKeyboard(m.LID_START_MESSAGE))
+                return (messages, h.current_step)
+            # get town
+            messages = []
+            messages.append(process_location_id(h, message))
+            if h.current_step == 'checkIn':
+                min_date = datetime.now().date()
+                calendar, step = WMonthTelegramCalendar(
+                    calendar_id=0,
+                    current_date=min_date,
+                    min_date=min_date,
+                    locale='ru'
+                ).build()
+                messages.append(
+                    MsgKeyboard(
+                        f'{m.CHECK_IN_START_MESSAGE} {LSTEP[step]}',
+                        markup=calendar))
+                return (messages, None)
+            return (messages, h.current_step)
+        case 'checkIn':
+            messages = []
+            messages.append(process_check_in(h, message))
+            if h.current_step == 'checkOut':
+                min_date = (h.checkIn + timedelta(days=1)).date()
+                calendar, step = WMonthTelegramCalendar(
+                    calendar_id=1,
+                    current_date=min_date,
+                    min_date=min_date,
+                    locale='ru'
+                ).build()
+                messages.append(
+                    MsgKeyboard(
+                        f'{m.CHECK_IN_START_MESSAGE} {LSTEP[step]}',
+                        markup=calendar))
+                return (messages, None)
+            return (messages, h.current_step)
+        case 'checkOut':
+            pass
+
+
+def process_location_id(h: History, location_name: str) -> MsgKeyboard:
+    # get town from db
+    locations_list = db.get_locations_by_name(location_name)
+    if len(locations_list) == 0:
+        # get town from api
+        try:
+            locations_list = api.get_locations(location_name)
+            # add locations to db
+            for location in locations_list:
+                if not db.add_location(location):
+                    logger.error('Error adding location to db.')
+        except api.ApiError:
+            locations_list = []
+    if len(locations_list) == 0:
+        # no town
+        return MsgKeyboard(m.LID_WRONG_MESSAGE)
+    if len(locations_list) == 1:
+        # one result
+        location = locations_list[0]
+        update_attrs = h.set_attributes(
+            {'locationId': location.destinationId})
+        db.update_history(h, update_attrs)
+        return MsgKeyboard(f'{m.LID_FINISH_MESSAGE}{location.name}')
     else:
-        print(h.id)
+        # more than one result
+        for location in locations_list:
+            if location.caption[:64] == location_name:
+                # one result
+                update_attrs = h.set_attributes(
+                    {'locationId': location.destinationId})
+                db.update_history(h, update_attrs)
+                return MsgKeyboard(f'{m.LID_FINISH_MESSAGE}{location.name}')
+        markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.row_width = min(3, len(locations_list))
+        for _i in range(markup.row_width):
+            caption = locations_list[_i].caption[:64]
+            markup.add(caption)
+        return MsgKeyboard(m.LID_ASK_MESSAGE, markup)
+
+
+def process_check_in(h: History, check_in) -> MsgKeyboard:
+    """Process check in date"""
+    dt = datetime(year=check_in.year, month=check_in.month, day=check_in.day)
+    update_attrs = h.set_attributes(
+        {'checkIn': dt})
+    db.update_history(h, update_attrs)
+    return MsgKeyboard(f"{m.CHECK_IN_FINISH_MESSAGE}{dt.strftime('%d.%m.%Y')}")
--- bot/handlers.py
+++ bot/handlers.py
@@ -1,13 +1,13 @@
 from telebot import TeleBot
-from telebot.types import Message
-# from telebot.types import InlineKeyboardButton
-# from telebot.types import InlineKeyboardMarkup
+from telebot.types import Message, CallbackQuery
 # from telebot.types import ReplyKeyboardMarkup
+# from telebot.types import InlineKeyboardMarkup
+# from telebot.types import InlineKeyboardButton
 # from telebot.types import InputMediaPhoto
 
-# from telegram_bot_calendar import WMonthTelegramCalendar
-# from telegram_bot_calendar import LSTEP
-# from datetime import datetime
+from telegram_bot_calendar import WMonthTelegramCalendar
+from telegram_bot_calendar import LSTEP
+from datetime import datetime
 
 from app.logger import get_logger
 from app.config import TOKEN
@@ -25,35 +25,56 @@ bot.load_next_step_handlers()
 bot.parse_mode = 'Markdown'
 
 
+def get_next_handler(current_step: str) -> object:
+    """Define next handler by current_step."""
+    return handler
+
+
 # Basic handlers
 @bot.message_handler(commands=['start'])
 def start_message(message: Message):
     """Process Start command."""
+    bot.send_chat_action(message.chat.id, 'typing')
     bot.send_message(message.chat.id, m.START_MESSAGE)
 
 
 @bot.message_handler(commands=['help'])
 def help_message(message: Message):
     """Process Help command."""
+    bot.send_chat_action(message.chat.id, 'typing')
     bot.send_message(message.chat.id, m.HELP_MESSAGE)
 
 
 @bot.message_handler(commands=['lowprice', 'highprice', 'bestdeal'])
 def basic_commands(message: Message):
     """Process Lowprice, Highprice and Bestdeal commands."""
-    s.process_command(message.chat.id, message.text)
+    bot.send_chat_action(message.chat.id, 'typing')
+    msgs, step = s.process_command(
+        message.chat.id, message.text)
+    for msg in msgs:
+        _m = bot.send_message(
+            message.chat.id, msg.msg, reply_markup=msg.markup)
+    if step:
+        bot.register_next_step_handler(_m, get_next_handler(step))
 
 
 @bot.message_handler(commands=['history'])
 def history_command(message: Message):
     """Process History command."""
+    bot.send_chat_action(message.chat.id, 'typing')
     pass
 
 
 @bot.message_handler(content_types=['text'])
 def check_text(message: Message):
     """Process unknown text."""
-    pass
+    bot.send_chat_action(message.chat.id, 'typing')
+    msgs, step = s.process_command(message.chat.id, message.text)
+    for msg in msgs:
+        _m = bot.send_message(
+            message.chat.id, msg.msg, reply_markup=msg.markup)
+    if step:
+        bot.register_next_step_handler(_m, get_next_handler(step))
 
 
 # Error handlers
@@ -67,9 +88,47 @@ def check_text(message: Message):
     'contact',
     'sticker'
 ])
-def error_message(message):
+def error_message(message: Message):
     """Any content without command"""
+    bot.send_chat_action(message.chat.id, 'typing')
     bot.reply_to(message, m.BAD_CONTENT_MESSAGE)
 
 
 # Next handlers
+def handler(message: Message):
+    """Handler to process user messages."""
+    bot.send_chat_action(message.chat.id, 'typing')
+    msgs, step = s.process_command(
+        message.chat.id, message.text)
+    for msg in msgs:
+        _m = bot.send_message(
+            message.chat.id, msg.msg, reply_markup=msg.markup)
+    if step:
+        bot.register_next_step_handler(_m, get_next_handler(step))
+
+
+@bot.callback_query_handler(func=WMonthTelegramCalendar.func(calendar_id=0))
+def callback_check_in(callback: CallbackQuery):
+    """Process checkIn callback."""
+    min_date = datetime.now().date()
+    chat_id = callback.message.chat.id
+    result, key, step = WMonthTelegramCalendar(
+        calendar_id=0,
+        current_date=min_date,
+        min_date=min_date,
+        locale='ru'
+    ).process(callback.data)
+    if not result and key:
+        bot.edit_message_text(
+            f'{m.CHECK_IN_START_MESSAGE} {LSTEP[step]}',
+            chat_id,
+            callback.message.message_id,
+            reply_markup=key
+        )
+    else:
+        print(result)
+        msgs, step = s.process_command(chat_id, result)
+        for msg in msgs:
+            _m = bot.send_message(chat_id, msg.msg, reply_markup=msg.markup)
+        if step:
+            bot.register_next_step_handler(_m, get_next_handler(step))
