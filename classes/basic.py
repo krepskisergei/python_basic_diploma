039f97cf6f598f802e7e0975962c0bd1ba16e4cf
Sergei Krepski <sergei@krepski.ru>
2022-10-25 17:51:33 +0300

    Add dataclasses

---
+++ classes/basic.py
@@ -0,0 +1,128 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=False)
+class Location:
+    """Dataclass for Location from API and database."""
+    destinationId: int
+    geoId: int
+    caption: str
+    name: str
+    name_lower: str = ''
+
+    def _format_content(self) -> None:
+        """Remove HTML tags from caption and generate name_lower."""
+        self.caption = self.caption.replace(
+            "<span class='highlighted'>", ""
+        ).replace("</span>", "")
+        self.name_lower = self.name.lower().replace(' ', '').replace('-', '')
+
+    @property
+    def data(self) -> list:
+        """Return entity data in list."""
+        self._format_content()
+        return [
+            self.destinationId,
+            self.geoId,
+            self.caption,
+            self.name,
+            self.name_lower
+        ]
+
+
+@dataclass(frozen=True)
+class Address:
+    """Dataclass for Hotel address from API."""
+    streetAddress: str
+    extendedAddress: str
+    locality: str
+    postalCode: str
+    region: str
+    countryName: str
+    countryCode: str
+    # TODO: add all attributes from API responce
+
+    def data(self, order: set = None) -> str:
+        """
+        Return entity data in string separated by commas.
+        Data order according set order.
+        """
+        order_attrs = (
+            'streetAddress',
+            'extendedAddress',
+            'locality',
+            'region',
+            'countryName'
+        )
+        if order is not None:
+            order_attrs = order
+        data_list = []
+        for _attr in order_attrs:
+            try:
+                value = self.__getattribute__(_attr)
+                if len(value) > 0:
+                    data_list.append(str(value))
+            except AttributeError:
+                pass
+        return ', '.join(data_list)
+
+
+@dataclass(frozen=True)
+class Hotel:
+    """Dataclass for Hotel from API and database."""
+    id: int
+    name: str
+    address: str
+    url: str
+    starRating: int
+    distance: str
+
+    @property
+    def data(self) -> list:
+        """Return entity data in list."""
+        return [
+            self.id,
+            self.name,
+            self.fullAddress,
+            self.url,
+            self.starRating,
+            self.distance
+        ]
+
+
+@dataclass(frozen=True)
+class HotelPhoto:
+    """
+    Dataclass for hotel photos from API and database.
+    Use format_url() before use data from entity.
+    """
+    imageId: int
+    hotelId: int
+    baseUrl: str
+
+    @property
+    def data(self) -> list:
+        """Return entity data in list."""
+        return [
+            self.imageId,
+            self.hotelId,
+            self.baseUrl
+        ]
+
+    def format_url(self, suffix: str) -> None:
+        """Replace {size} to suffix in baseUrl."""
+        self.baseUrl = self.baseUrl.replace('{size}', suffix)
+
+
+@dataclass(frozen=True)
+class SearchResult:
+    hotel: Hotel
+    price: float
+
+    @property
+    def data(self) -> list:
+        """Return entity data in list."""
+        return [
+            self.hotel.id,
+            self.price
+        ]
