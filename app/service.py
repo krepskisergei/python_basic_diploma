32c86ed0644b310aa6d6099c7dcfecb7ab106fea
Sergei Krepski <sergei@krepski.ru>
2022-10-28 14:49:24 +0300

    Add ReplyMessage dataclass, get_session method

--- app/service.py
+++ app/service.py
@@ -1,8 +1,12 @@
+from dataclasses import dataclass
+from telebot.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+
 from app.app_logger import get_logger
 from app.config import DB_ENGINE
 from classes.basic import Hotel, HotelPhoto, Location
 from classes.database import DB
 from classes.hotels_api import HotelsApi
+from classes.user_session import UserSession
 
 
 # initiate logger
@@ -10,6 +14,17 @@ logger = get_logger(__name__)
 # initiate database and api
 db = DB(DB_ENGINE)
 api = HotelsApi()
+
+
+# Classes
+@dataclass(frozen=True)
+class ReplyMessage:
+    """Dataclass for reply messages."""
+    chat_id: int
+    edit: bool = False
+    text: str = None
+    markup: ReplyKeyboardMarkup | InlineKeyboardMarkup = None
+    media: list = None
 
 
 # Api and database unify methods
@@ -50,3 +65,20 @@ def get_hotel_photos(hotel: Hotel, limit: int = 0) -> list[HotelPhoto]:
     if 0 < limit < len(photos):
         return photos[:limit]
     return photos
+
+
+# Session methods
+def get_session(chat_id: int, command: str = None) -> UserSession:
+    """
+    Return UserSession by chat_id from database.
+    Create new UserSession instance if command is None and is correct.
+    """
+    session = db.get_active_session(chat_id)
+    if session is not None:
+        return session
+    # no active session in database
+    if command is None:
+        return None
+    session_attrs = {'command': command}
+    session = UserSession(chat_id, **session_attrs)
+    return db.add_session(session)
