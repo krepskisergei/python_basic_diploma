cb967fd592e0fec963558a52f06d934bbcd04a44
Sergei Krepski <sergei@krepski.ru>
2022-10-29 00:38:51 +0300

    First run test

--- main.py
+++ main.py
@@ -23,25 +23,10 @@ if __name__ == '__main__':
         clear_debug_log()
 
         from app.app_logger import get_logger
+        from bot.handlers import bot
+
+        # initiate logger
         logger = get_logger(__name__)
 
-        logger.debug('Debug.')
-        logger.info('Application starts.')
-
-        import app.service as s
-        from datetime import date
-
-        session = s.get_session_bychatid(1)
-        attrs = {
-            'location_id': 123,
-            'check_in': date(2022, 11, 1),
-            'check_out': date(2022, 11, 3),
-            'price_min': 0.0,
-            'price_max': 0.0,
-            'distance_min': 0.0,
-            'distance_max': 0.0,
-            'results_num': 4,
-            'photos_num': 0
-        }
-        print(session.set_attrs(attrs))
-        print(session.current_step)
+        logger.info('Application starts...')
+        bot.polling(non_stop=True, interval=0)
